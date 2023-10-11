010e05514468f9f70d820ad32d7555a53da3e0d5
manuelcalle <manuel.calle@udea.edu.co>
2023-10-11 08:50:06 -0500

    Exponer api al front CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Configura las opciones de CORS
+  app.enableCors({
+    origin: 'http://localhost:3001', // Reemplaza con el origen de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Habilita las cookies y encabezados de autorización
+  });
+
   await app.listen(3000);
 }
 bootstrap();
